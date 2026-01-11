0f16e42b32837778d0c6f28000eee2a617ebf726
Anurag <anuragsingh22324@gmail.com>
2026-01-11 14:05:14 +0530

    first commit project created

---
+++ src/main/java/com/anurag/datapi/DatapiApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.datapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anurag/datapi/DatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.datapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
